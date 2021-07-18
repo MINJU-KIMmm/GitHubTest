bcad89627595a1aac7ca3c4e59f1df7a5cd1061a
MINJU-KIMmm <0112km@ewhain.net>
2021-07-18 22:33:23 +0900

    Update mainBranch.java

--- mainBranch.java
+++ mainBranch.java
@@ -3,7 +3,7 @@ public class mainBranch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is a main Branch");
+		System.out.println("this is not a main Branch");
 		System.out.println("this is a sub Branch");
 	}
 
